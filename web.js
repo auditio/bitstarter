67d75f2469836d64beb2dfc86b694cca0e144bcd
Reejuta Sharmin <mail@auditio.org>
2013-07-19 06:14:05 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
+var fs = require ('fs');
 app.use(express.logger());
 
+var buf = new Buffer(100);
+
+fs.readFile('index.html', function read (err, data){
+    if(err){ 
+       throw err;
+    }
+
+    buf = data;
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
